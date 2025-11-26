f3c0a1accda0d5a208089ff1bfabbf487867da13
MarosMincak <maros.mincak11@gmail.com>
2025-11-26 09:27:16 +0100

    Refactor card generation and display logic in 11karty.php

--- 11karty.php
+++ 11karty.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form method="post">
+    <input type="submit" name="rozdaj" value="Rozdaj">
+  </form>
+</body>
+</html>
 <?php
 $karty = [
   "2"  => 2,
@@ -15,12 +28,13 @@ $karty = [
   "A"  => 11,
 ];
 
-$znaky = ["♦","♥","♠","♣"];
+$znaky = ["♦", "♥", "♠", "♣"];
 
 $mojeKarty = [];
 $dealeroveKarty = [];
 
-function generateCards(){
+function generateCards()
+{
   global $znaky, $karty;
   $randomKarta = array_rand($karty, 1);
   $randomZnak = $znaky[random_int(0, count($znaky) - 1)];
@@ -29,5 +43,28 @@ function generateCards(){
   return $karta;
 }
 
-echo generateCards();
+// Generovanie prvej karty pre dealera
+array_push($dealeroveKarty, generateCards());
+
+// Generovanie prvej karty pre mňa
+array_push($mojeKarty, generateCards());
+
+// Generovanie druhej karty pre mňa
+array_push($mojeKarty, generateCards());
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["rozdaj"])){
+  rozdajKarty($dealeroveKarty);
+  echo "<br>";
+  rozdajKarty($mojeKarty);
+}
+
+function rozdajKarty($karty){
+  if(!empty($karty)){
+    echo "<h1>Karty: </h1>" ;
+    foreach($karty as $karta){
+      echo "<h2>" . $karta . " " . "</h2>";
+    }
+  }
+}
 
